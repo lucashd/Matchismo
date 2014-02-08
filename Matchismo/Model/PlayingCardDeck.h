48362acea36aa1c3e27647670d3281e5c32fe3f3
lucashd <lhdavid15@gmail.com>
2014-02-07 23:17:37 -0800

    Update PlayingCardDeck.h

--- Matchismo/Model/PlayingCardDeck.h
+++ Matchismo/Model/PlayingCardDeck.h
@@ -2,8 +2,8 @@
 //  PlayingCardDeck.h
 //  Matchismo
 //
-//  Created by Lucas David on 8/27/13.
-//  Copyright (c) 2013 xalusc. All rights reserved.
+//  Created by Lucas David
+//  Copyright (c) 2014 lucashd. All rights reserved.
 //
 
 #import "Deck.h"
