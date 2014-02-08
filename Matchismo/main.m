69d917b904e91ab2f6c98996cb47f65901775a02
lucashd <lhdavid15@gmail.com>
2014-02-07 23:13:32 -0800

    Update main.m

--- Matchismo/main.m
+++ Matchismo/main.m
@@ -3,7 +3,7 @@
 //  Matchismo
 //
 //  Created by Lucas David on 8/24/13.
-//  Copyright (c) 2013 xalusc. All rights reserved.
+//  Copyright (c) 2013 lucashd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
